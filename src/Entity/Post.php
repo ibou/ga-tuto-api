0007a9decc2145bd9c09023bb0ac7a1ad2444b64
Ibrahima D <ibrahima.diallo@display-interactive.com>
2021-04-25 20:34:43 +0200

    first commit

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,11 +5,22 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=PostRepository::class)
- * @ApiResource()
  */
+#[ApiResource(
+    itemOperations: [
+    'get' => [
+        'normalization_context' => ['groups' => ['read:collection', 'read:item', 'read:Post']],
+    ],
+    'put',
+    'delete',
+],
+    denormalizationContext: ['groups' => ['write:Post']],
+    normalizationContext: ['groups' => ['read:collection']],
+)]
 class Post
 {
     /**
@@ -17,95 +28,126 @@ class Post
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
-
+    #[Groups(['read:collection'])]
+    private int $id;
+    
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $title;
-
+    #[Groups(['read:collection', 'write:Post'])]
+    private string $title;
+    
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $slug;
-
+    #[Groups(['read:collection', 'write:Post'])]
+    private string $slug;
+    
     /**
      * @ORM\Column(type="text")
      */
-    private $content;
-
+    #[Groups(['read:item', 'write:Post'])]
+    private string $content;
+    
     /**
      * @ORM\Column(type="datetime")
      */
-    private $createdAt;
-
+    #[Groups(['read:item'])]
+    private \DateTimeInterface $createdAt;
+    
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="datetime", nullable=true)
      */
-    private $updatedAt;
-
+    private \DateTimeInterface $updatedAt;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="posts")
+     */
+    #[Groups(['read:item', 'write:Post'])]
+    private ?Category $category = null;
+    
+    
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+    
+    
     public function getId(): ?int
     {
         return $this->id;
     }
-
+    
     public function getTitle(): ?string
     {
         return $this->title;
     }
-
+    
     public function setTitle(string $title): self
     {
         $this->title = $title;
-
+        
         return $this;
     }
-
+    
     public function getSlug(): ?string
     {
         return $this->slug;
     }
-
+    
     public function setSlug(string $slug): self
     {
         $this->slug = $slug;
-
+        
         return $this;
     }
-
+    
     public function getContent(): ?string
     {
         return $this->content;
     }
-
+    
     public function setContent(string $content): self
     {
         $this->content = $content;
-
+        
         return $this;
     }
-
+    
     public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
     }
-
+    
     public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
-
+        
         return $this;
     }
-
+    
     public function getUpdatedAt(): ?\DateTimeInterface
     {
         return $this->updatedAt;
     }
-
+    
     public function setUpdatedAt(\DateTimeInterface $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
-
+        
+        return $this;
+    }
+    
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+    
+    public function setCategory(?Category $category = null): self
+    {
+        $this->category = $category;
+        
         return $this;
     }
 }
