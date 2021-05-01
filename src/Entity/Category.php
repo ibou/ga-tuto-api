ba049265c0be49e89d3217d4c4271de13c499639
Ibrahima D <ibrahima.diallo@display-interactive.com>
2021-05-01 14:08:34 +0200

    Validation data

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\Length;
 
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
@@ -28,7 +29,10 @@ class Category
     /**
      * @ORM\Column(type="string", length=255)
      */
-    #[Groups(['read:Post'])]
+    #[
+        Groups(['read:Post', 'write:Post']),
+        Length(min: 5)
+    ]
     private ?string $name;
 
     /**
