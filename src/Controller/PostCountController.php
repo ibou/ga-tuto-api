429ed86cf34d6240724d441efab67754e141d14e
Ibrahima D <ibrahima.diallo@display-interactive.com>
2021-07-25 03:52:54 +0200

    open

---
+++ src/Controller/PostCountController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Repository\PostRepository;
+use Symfony\Component\HttpFoundation\Request;
+
+class PostCountController
+{
+    public function __construct(private PostRepository $postRepository)
+    {
+    }
+    
+    public function __invoke(Request $request): int
+    {
+        $onlineQuery = $request->get('online');
+        $condition = [];
+        if (null !== $onlineQuery) {
+            $condition = ['online' => '1' === $onlineQuery];
+        }
+        
+        return $this->postRepository->count($condition);
+    }
+}
