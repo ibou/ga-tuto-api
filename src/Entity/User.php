9adf031f7952100b48a74a452140d9bee7ee2b94
Ibrahima D <ibrahima.diallo@display-interactive.com>
2021-08-06 22:31:48 +0200

    creation tocken jwt

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use App\Controller\MeController;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\User\JWTUserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -16,71 +17,88 @@ use Symfony\Component\Serializer\Annotation\Groups;
  */
 #[ApiResource(
     collectionOperations: [
-],
+    ],
     itemOperations: [
-    'get' => [
-        'controller' => NotFoundAction::class,
-        'openapi_context' => ['summary' => 'hidden'],
-        'read' => false,
-        'output' => false,
-    ], 'me' => [
+        'get' => [
+            'controller' => NotFoundAction::class,
+            'openapi_context' => ['summary' => 'hidden'],
+            'read' => false,
+            'output' => false,
+        ],
+        'me' => [
             'pagination_enabled' => false,
             'path' => '/me',
             'method' => 'get',
             'controller' => MeController::class,
             'read' => false,
             'openapi_context' => [
-                'security' => ['cookieAuth' => ['']],
+                'security' => [['bearerAuth' => []]],
             ],
         
-        ]
-],
+        ],
+    ],
     normalizationContext: ['groups' => ['read:user']],
     security: 'is_granted("ROLE_USER")'
 )]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface, JWTUserInterface
 {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private ?int $id;
-
+    private ?string $id;
+    
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      */
     #[Groups(['read:user'])]
-    private string $email;
-
+    private ?string $email = null;
+    
     /**
      * @ORM\Column(type="json")
      */
+    #[Groups(['read:user'])]
     private array $roles = [];
-
+    
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
      */
     private string $password;
-
-    public function getId(): ?int
+    
+    public static function createFromPayload($id, array $payload)
+    {
+        return (new User())->setId($id)->setEmail($payload['username'] ?? '');
+    }
+    
+    public function getId(): ?string
     {
         return $this->id;
     }
-
+    
+    /**
+     * @param string|null $id
+     */
+    public function setId(?string $id): self
+    {
+        $this->id = $id;
+        
+        return $this;
+    }
+    
     public function getEmail(): ?string
     {
         return $this->email;
     }
-
-    public function setEmail(string $email): self
+    
+    public function setEmail(string $email = null): self
     {
         $this->email = $email;
-
+        
         return $this;
     }
-
+    
     /**
      * A visual identifier that represents this user.
      *
@@ -90,7 +108,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return (string)$this->email;
     }
-
+    
     /**
      * @deprecated since Symfony 5.3, use getUserIdentifier instead
      */
@@ -98,7 +116,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return (string)$this->email;
     }
-
+    
     /**
      * @see UserInterface
      */
@@ -107,17 +125,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
-
+        
         return array_unique($roles);
     }
-
+    
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
-
+        
         return $this;
     }
-
+    
     /**
      * @see PasswordAuthenticatedUserInterface
      */
@@ -125,14 +143,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->password;
     }
-
+    
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
+        
         return $this;
     }
-
+    
     /**
      * Returning a salt is only needed, if you are not using a modern
      * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
@@ -143,7 +161,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return null;
     }
-
+    
     /**
      * @see UserInterface
      */
