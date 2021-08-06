d0be95bdc55e1ea2daf3f05fe580a01b0228c364
Ibrahima D <ibrahima.diallo@display-interactive.com>
2021-08-06 15:54:01 +0200

    auth form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/api/login', name: 'api_login', methods: ['POST'])]
-    public function login()
+    public function apiLogin()
     {
         $user = $this->getUser();
         return $this->json([
@@ -16,10 +18,29 @@ class SecurityController extends AbstractController
             'roles' => $user->getRoles(),
         ]);
     }
-    
-    #[Route(path: '/api/logout', name: 'api_logout',methods: ['POST'])]
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
     public function logout()
     {
-    
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 }
